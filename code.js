34919c57b5c6a598c0ca7d531c26f9bfeae8220c
katiiisha <mendeleevakate2002@gmail.com>
2022-12-15 23:23:49 +0300

    не понимаю как проверить работает ли функция

--- code.js
+++ code.js
@@ -1,7 +1,19 @@
-// задача 1 
-function addition(numbaer1, number2) {
-    
-    console.log(`${numbaer1}+${number2}`)
-}
+// задача 1
 let numbaer1 = prompt()
 let number2 = prompt()
+function addition(numbaer1, number2) { 
+    return numbaer1 + number2
+}
+
+// задача 2 
+//если дано одно слово
+
+palindrome('strong');
+palindrome('level');
+function palindrome(str) {
+    str = str.toLowerCase(); // не учитывается регистр     
+    return str === str.split('').reverse().join('');
+}
+
+
+
