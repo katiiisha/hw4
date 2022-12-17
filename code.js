7e5431307f57fb8a6353a91f0e7c154100eeab3d
katiiisha <115351618+katiiisha@users.noreply.github.com>
2022-12-17 17:20:27 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
 // задача 1
-let numbaer1 = +prompt()
-let number2 = +prompt()
-function addition(numbaer1, number2) { 
-    return numbaer1 + number2
+const num1 = +prompt();
+const num2 = +prompt();
+
+function getSumm(num1, num2) {
+    return num1+num2
 }
+alert (getSumm(num1,num2))
+
 
 // задача 2 
 //если дано одно слово
