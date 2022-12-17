618adc924d9c1b3d18a751034fc5f35483222a69
katiiisha <115351618+katiiisha@users.noreply.github.com>
2022-12-17 17:41:03 +0300

    Update code.js

--- code.js
+++ code.js
@@ -10,13 +10,24 @@ alert (getSumm(num1,num2))
 
 // задача 2 
 //если дано одно слово
+//вариант 1 
 palindrome('strong');
 palindrome('level');
 function palindrome(str) {
     str = str.toLowerCase(); // не учитывается регистр
     return (str === str.split('').reverse().join('')) ? console.log("палиндом") : console.log("не палиндром ")
 }
-
+// вариант 2 
+function checkPoly(str) {
+    let reverStr = ''
+    for (let i = str.length - 1; i >= 0; i--){
+        reverStr += str[i]
+    }
+    return reverStr.toLowerCase() === str.toLowerCase()   
+}
+//вариант 3
+const checkPoly = (str) => str.toLowerCase() === str.split('').reverse().join('')
+console.log(checkPoly('level'))
 
 
 
